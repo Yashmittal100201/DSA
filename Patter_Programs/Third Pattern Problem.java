7333c85c6015ff09200fbec08a4a0e9f0460677c
Yashmittal100201 <82879010+Yashmittal100201@users.noreply.github.com>
2021-11-27 15:36:09 +0530

    Create Third Pattern Problem.java

---
+++ Patter_Programs/Third Pattern Problem.java
@@ -0,0 +1,28 @@
+//    To print pattern like this
+
+//    *
+//    * *
+//    * * *
+//    * * * *
+//    * * * * *
+//    * * * *
+//    * * *
+//    * * 
+//    *
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n = 9;
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j = 1; j <= ( i <= (n+1)/2 ? i : n-i+1 ); j++)
+	        {
+	            
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
