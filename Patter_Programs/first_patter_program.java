88ab83ffc73c473b098ace81b65563906beee0d9
Yashmittal100201 <82879010+Yashmittal100201@users.noreply.github.com>
2021-11-27 15:00:47 +0530

    Update first_patter_program.java

--- Patter_Programs/first_patter_program.java
+++ Patter_Programs/first_patter_program.java
@@ -10,9 +10,9 @@ public class Main
 {
 	public static void main(String[] args) {
 	    int n = 4;
-	    for(int i=0;i<n;i++)
+	    for(int i=1;i<=n;i++)
 	    {
-	        for(int j=n-i;j>0;j--)
+	        for(int j=1;j<=n-i+1;j++)
 	        {
 	            System.out.print("* ");
 	        }
